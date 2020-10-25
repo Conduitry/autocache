b15dd62139955c58ff396df77df90e362dfab9c6
Conduitry <git@chor.date>
2020-10-24 22:59:07 -0400

    version 0.1.3 (release)

---
+++ dist/index.esm.js
@@ -0,0 +1,59 @@
+import { createHash } from 'crypto';
+import { promises, writeFileSync } from 'fs';
+import { resolve } from 'path';
+import { deserialize, serialize } from 'v8';
+
+const autocache = (path, mode) => {
+    path = resolve(path);
+    let cache = new Map();
+    const removing = new Set();
+    const pending = new Map();
+    const loading = promises
+        .readFile(path)
+        .then(deserialize)
+        .then((data) => {
+        if (data && typeof data === 'object' && data.schema === 1 && data.cache instanceof Map) {
+            cache = data.cache;
+            for (const [key, [, modes]] of cache) {
+                modes.delete(mode);
+                if (modes.size === 0) {
+                    removing.add(key);
+                }
+            }
+        }
+    })
+        .catch(() => { });
+    return {
+        async cache(key, compute_value) {
+            await loading;
+            const key_hashed = createHash('sha1').update(key).digest('hex');
+            let value, modes;
+            if (cache.has(key_hashed)) {
+                [value, modes] = cache.get(key_hashed);
+            }
+            else if (pending.has(key_hashed)) {
+                return pending.get(key_hashed);
+            }
+            else {
+                const promise = compute_value();
+                pending.set(key_hashed, promise);
+                value = await promise;
+                modes = new Set();
+                cache.set(key_hashed, [value, modes]);
+                pending.delete(key_hashed);
+            }
+            removing.delete(key_hashed);
+            modes.add(mode);
+            return value;
+        },
+        close() {
+            for (const key of removing) {
+                cache.delete(key);
+            }
+            writeFileSync(path, serialize({ schema: 1, cache }));
+        },
+    };
+};
+
+export { autocache };
+//# sourceMappingURL=index.esm.js.map
